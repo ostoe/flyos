b19037f05969832326a752f7da7eac27fda49010
linhaifly@163.com <linhaifly@163.com>
2023-04-24 17:55:55 +0800

    bcd

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,4 +1,6 @@
-use core::alloc::{Layout, GlobalAlloc};
+use core::{alloc::{Layout, GlobalAlloc}, ptr::NonNull};
+
+use super::Locked;
 
 struct ListNode {
     next: Option<&'static mut ListNode>,
@@ -18,11 +20,12 @@ impl FixedSizeBlockAllocator {
             fallback_allocator: linked_list_allocator::Heap::empty(),
         }
     }
-
+    // 出事后heap大小
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
     }
 
+    /// 在heap 区域上直接分配 layout大小的内存
     fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
         match self.fallback_allocator.allocate_first_fit(layout) {
             Ok(ptr) => ptr.as_ptr,
@@ -41,13 +44,44 @@ unsafe impl  GlobalAlloc for Locked<FixedSizeBlockAllocator> {
         let mut allocator = self.lock();
         match list_index(&layout) {
             Some(index) => {
-                match allocator
-            }
+                // 创建的时候，如果已经有节点，就从节点里面取出来，不用调用底层分配了。
+                match allocator.list_heads[index].take() {
+                    Some(node) => {
+                        allocator.list_heads[index] = node.next.take();
+                        node as *mut ListNode as *mut u8
+                    },
+                    /// ListNode链表为空：
+                    None => {
+                        let block_size = BLOCK_SIZES[index];
+                        let block_align = block_size;
+                        let layout = Layout::from_size_align(block_size, block_align).unwrap();
+                        allocator.fallback_alloc(layout)
+                    }
+                }
+            },
+            None => allocator.fallback_alloc(layout),
         }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        
+        let mut allocator = self.lock();
+        match list_index(&layout) {
+            // 回收的时候，新建一个节点，插入头部，作为回收链表池！
+            Some(index) => {
+                let new_node = ListNode {
+                    next: allocator.list_heads[index].take()
+                };
+                assert!(mem::size_of::<ListNode>() <= BLOCK_SIZES[index]);
+                assert!(mem::align_of::<ListNode>() <= BLOCK_SIZES[index]);
+                let new_node_ptr = ptr as *mut ListNode;
+                new_node_ptr.write(new_node);
+                allocator.list_heads[index] = Some(&mut *new_node_ptr);
+            },
+            None => {
+                let ptr = NonNull::new(ptr).unwrap();
+                allocator.fallback_allocator.deallocate(ptr, layout);
+            }
+        }
     }
     
 }
